ed656035fbe83d7e7fa593832aa2c16191bdf250
Aman Kumar Ayush <amankumar.ayush@thoughtworks.com>
2022-08-24 12:34:29 +0530

    Refactor. download models script file

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -1,46 +1,120 @@
-#!/bin/bash
+#! /bin/bash 
 
 cd "$(dirname "$0")"/..
+shopt -s nocasematch
+
+vakhyash_api_host_url="vakhyash-api:50051"
+max_connection_to_proxy_server="10"
+supported_languages=(english hindi tamil)
+
+english=0
+hindi=1
+tamil=2
+supported_languages_code=([english]="en" [hindi]="hi" [tamil]="ta")
+
+model_dict_data=""
+language_map_data=""
+
+update_model_dict_data () {
+    if [[ $model_dict_data != "" ]]; then
+       model_dict_data+=","
+    fi
+    data="\"$1\": {
+        \"path\": \"/$2/$2_infer.pt\",
+        \"enablePunctuation\": true,
+        \"enableITN\": true
+    }"
+    model_dict_data+="$data"
+}
+
+write_model_dict_file () {
+    data="{ $1 }"
+    echo "$data" > model_dict.json
+}
+
+update_language_map_data () {
+    if [[ $language_map_data != "" ]]; then
+       language_map_data+=", "
+    fi
+    language_map_data+="\"$1\""
+}
+
+write_language_map_file () {
+    data="{
+    \"$1\": {
+      \"languages\": [
+        "$2"
+      ],
+      \"maxConnectionCount\": $3
+    }
+}"
+    echo "$data" > language_map.json
+}
+
+
+
+isLanguageSupported () {
+   for i in "${!supported_languages[@]}"; do
+     if [[ ${supported_languages[$i]} = $1 ]]; then
+       return 1
+     fi
+   done
+   return 0
+}
+
+downloadFile () {
+    code=$(curl -O -w "%{http_code}" $1)
+    if [[ $code != 200  ]] ; then
+        echo "unable to download file : $1"
+        exit 1
+     fi 
+}
+    
+downloadLanguageModelFiles () {
+    dictFilePath="https://storage.googleapis.com/vakyansh-open-models/models/$1/$2-IN/dict.ltr.txt"
+    inferFilePath="https://storage.googleapis.com/vakyansh-open-models/models/$1/$2-IN/$1_infer.pt"
+    lexiconFilePath="https://storage.googleapis.com/vakyansh-open-models/models/$1/$2-IN/lexicon.lst"
+    binaryFilePath="https://storage.googleapis.com/vakyansh-open-models/models/$1/$2-IN/lm.binary"
+
+    file_path_arr=($dictFilePath $inferFilePath $lexiconFilePath $binaryFilePath)
+
+    for i in "${!file_path_arr[@]}"; do
+        downloadFile ${file_path_arr[$i]}
+    done
+}
+
+languages="$1"
+
+for lang in $languages; do
+    isLanguageSupported $lang
+
+    response=$?
+
+    if [[ $response == 0  ]] ; then
+        echo "language is not supported : $lang"
+        exit 1
+    fi
+done
 
 mkdir -p api/deployed_models && cd "$_"
 
-echo "downloading english models..."
+for lang in $languages; do
 
-mkdir english_test && cd "$_"
+    language=$(echo $lang | tr '[:upper:]' '[:lower:]')
 
-code=$(curl -O -w "%{http_code}" https://storage.googleapis.com/vakyansh-open-models/models/english/en-IN/dict.ltr.txt)
+    mkdir -p "$language" && cd "$_"
 
-if [[ $code != 200  ]] ; then
-    echo "Erorr occured while downloading dictionary file."
-fi
+    downloadLanguageModelFiles $language ${supported_languages_code[language]}
 
-code=$(curl -O -w "%{http_code}" https://storage.googleapis.com/vakyansh-open-models/models/english/en-IN/english_infer.pt)
+    cd .. 
+    update_model_dict_data ${supported_languages_code[language]} $language 
+    update_language_map_data ${supported_languages_code[language]}
+done
 
-if [[ $code != 200  ]] ; then
-    echo "Erorr occured while downloading inference file."
-fi
+write_model_dict_file "$model_dict_data"
 
-code=$(curl -O -w "%{http_code}" https://storage.googleapis.com/vakyansh-open-models/models/english/en-IN/lexicon.lst)
+cd ../..
 
-if [[ $code != 200  ]] ; then
-    echo "Erorr occured while downloading lexicon file."
-fi
+mkdir -p proxy/deployed_models && cd "$_"
 
-code=$(curl -O -w "%{http_code}" https://storage.googleapis.com/vakyansh-open-models/models/english/en-IN/lm.binary)
-
-if [[ $code != 200  ]] ; then
-    echo "Erorr occured while downloading lm file."
-fi
-
-cd ..
-
-echo "downloading hindi models..."
-
-mkdir hindi && cd "$_"
-
-code=$(curl -O https://storage.googleapis.com/vakyansh-open-models/models/hindi/hi-IN/dict.ltr.txt)
-if [[ $code != 200  ]] ; then
-    echo "Erorr occured while downloading dictionary file."
-fi
-
-echo "download successful !!."
+write_language_map_file "$vakhyash_api_host_url" "$language_map_data" $max_connection_to_proxy_server
